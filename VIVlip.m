8196d44eb43c04c32ea84a4b3e5256cc37d90b52
SilvinWillemsen <silvinwillemsen@gmail.com>
2020-08-31 12:43:50 +0200

    Added theta (heaviside variable). Creating elephant sounds

--- VIVlip.m
+++ VIVlip.m
@@ -1,13 +1,13 @@
 clear all;
 close all;
-drawSpeed = 10000;
+drawSpeed = 1;
 
 fs = 44100;
 k = 1 / fs;
 lengthSound = fs * 10;
 
 f0 = 220;                   % fundamental freq lips
-m = 0.001;                  % mass lips
+M = 0.001;                  % mass lips
 omegaSq = (2 * pi * f0)^2;  % angular freq
 
 %% viscothermal effects
@@ -18,7 +18,7 @@ eta = 1.846 * 1e-5 * (1 + 0.0025 * tempAbove2685);  % shear viscosity
 nu = 0.8410 * (1 - 0.0002 * tempAbove2685);         % root of prandtl number
 gamma = 1.4017 * (1 - 0.00002 * tempAbove2685);     % ratio of specific heats
     
-K = omegaSq * m;            % spring constant lip
+K = omegaSq * M;            % spring constant lip
 Pm = 20;                    % Blowing pressure (kPa)
 Rspring = 0.23;             % initial damping
 x0 = (0.017 * Pm / 100 + 0.23) * 1e-3;       % mean displacement
@@ -26,7 +26,7 @@ q = 2.0;                    % exponent value
 Ab = 1e-6;                  % area bottom (m^2)
 
 wR = 0.33;
-z = 0;
+z = -0.1;
 zPrev = z;
 prevP = 1;
 for n = 1:lengthSound
@@ -38,10 +38,11 @@ for n = 1:lengthSound
 %     if n > lengthSound / 2
 %         Pm = 0;
 %     end
-
+%     theta = 0;
+    
     R = max (Rspring - wR * abs (x0 / z)^q, 0);
     Rsave(n) = R;
-    
+    R = 0;
     A = c * (1 - theta) * sqrt(2 * rho);
     p1 = Pm + ((-A + sqrt(A^2 - 4 * Pm)) / 2)^2;
     p2 = Pm - ((-A + sqrt(A^2 + 4 * Pm)) / 2)^2;
@@ -61,12 +62,24 @@ for n = 1:lengthSound
     end
     pSave(n) = p;
     Fsave(n) = gamma * (Pm - p) + Ab * (1 - theta) * p;
-    zNext(n) = (Fsave(n) + (2 * m / k^2 - (1 + 3 * theta) * K) * z + ((1 + 4 * theta) * R / (2 * k) - m / k^2) * zPrev) / (m / k^2 + (1 + 4 * theta) * R / (2 * k));
-
+    Fsave(n) = 0;
+    zNext(n) = (Fsave(n) + (2 * M / k^2) * z + ((1 + 4 * theta) * R / (2 * k) - M / k^2 - (1 + 3 * theta) * K / 2) * zPrev) / (M / k^2 + (1 + 4 * theta) * R / (2 * k) + (1 + 3 * theta) * K / 2);
+%     zNext(n) = 2 * z - zPrev - K / M * k^2 * z;
+    kinEnergy(n) = M / 2 * sum((1/k * (z - zPrev)).^2);
+    potEnergy(n) = (1 + 3 * theta) * K / 2 * 1/2 * (z^2 + zPrev^2);
+    totEnergy(n) = kinEnergy(n) + potEnergy(n);
 %     zNext(n) = (2 * z - zPrev - (1 + 3 * theta) * omegaSq * k^2 * z + (1 + 4 * theta) * R * k^2 / 2 * zPrev) / (1 + (1 + 4 * theta) * R * k^2 / 2);
     
     if mod(n, drawSpeed) == 0
+        subplot(211)
         plot(zNext(1:n))
+        subplot(212)
+        plot(totEnergy(1:n) / totEnergy(1) - 1)
+%         hold off
+%         plot(kinEnergy(1:n))
+%         hold on
+%         plot(potEnergy(1:n))
+
         drawnow;
     end
     
--- firstOrderBrassWithLip.m
+++ firstOrderBrassWithLip.m
@@ -6,21 +6,21 @@ clear all;
 close all;
 
 % drawing variables
-drawThings = true;
-drawSpeed = 10000;
+drawThings = false;
+drawSpeed = 100000;
 centered = true;
 
 impulse = false;
 
 fs = 44100;         % Sample rate (Hz)
 k = 1/fs;           % Time step (s)
-lengthSound = fs * 2; % Duration (s)
+lengthSound = fs * 5; % Duration (s)
 
 %% Tube variables
 c = 343;            % Wave speed (m/s)
 rho = 1;
 h = c * k;          % Grid spacing (m)
-L = 1;              % Length
+L = 3;              % Length
 
 N = floor(L/h);             % Number of points (-)
 h = L/N;                    % Recalculate gridspacing from number of points
@@ -33,15 +33,15 @@ a1 = 1; % loss
 [S, SHalf, SBar] = setTube (N);
 
 %% Lip variables
-f0 = 200;                   % fundamental freq lips
+f0 = 214;                   % fundamental freq lips
 M = 5.37e-5;                  % mass lips
-omega0 = 2 * pi * f0;  % angular freq
+omega0Init = 2 * pi * f0;  % angular freq
 
 %% viscothermal effects
 T = 26.85;
 [c, rho, eta, nu, gamma] = calcThermoDynConstants(T);
   
-sig = 1;
+sig = 0.01;
 H0 = 2.9e-4;
 
 y = H0;
@@ -56,7 +56,7 @@ p = zeros(N, 1);
 vNext = zeros(N-1, 1);
 v = zeros(N-1, 1);
 
-amp = 0;
+amp = 100;
 % if ~impulse  
 %     % input signal
 %     t = (0:lengthSound - 1) / fs;
@@ -98,8 +98,14 @@ totEnergy = zeros (lengthSound, 1);
 qHReed = zeros (lengthSound, 1);
 pHReed = zeros (lengthSound, 1);
 for n = 1:lengthSound
-    
-%     omega0 = 2 * pi * f0 * (1 + 5 * n / lengthSound);
+    if y > 0
+        theta = 0;
+    else
+        theta = 1;
+    end
+%     theta = 0;
+    omega0Init = 2 * pi * f0 * (2 + 0.01 * sin(20 * pi * n / lengthSound));
+    omega0 = omega0Init * sqrt(1 + 3 * theta);
 %     omega0Save(n) = omega0;
     %% Calculate velocities before lip model
     vNext(vRange) = v(vRange) - lambda / (rho * c) * (p(vRange+1) - p(vRange));
@@ -107,22 +113,22 @@ for n = 1:lengthSound
     %% Lip model
     
     % if lipstate is below 0, change heaviside variable to 1
-    if y > 0
-        theta = 0;
-    else
-        theta = 1;
-    end
-    
-%     ramp = 10000;
-%     if n < ramp
-%         Pm = amp * n / ramp;
+%     if y > 0
+%         theta = 0;
 %     else
-        Pm = amp;
+%         theta = 1;
 %     end
+    
+    ramp = 100;
+    if n < ramp
+        Pm = amp * n / ramp;
+    else
+        Pm = amp;
+    end
     
     a1 = 2 / k + omega0^2 * k + sig;
     a2 = Sr / M;
-    a3 = 2/k * 1/k * (y - yPrev) - omega0^2 * yPrev;
+    a3 = 2/k * 1/k * (y - yPrev) - omega0^2 * yPrev + omega0^2 * H0;
     b1 = SHalf(1) * vNext(1) + h * SBar(1) / (rho * c^2 * k) * (Pm  - p(1));
     b2 = h * SBar(1) / (rho * c^2 * k);
     c1 = w * subplus(y + H0) * sqrt(2 / rho);
@@ -134,8 +140,8 @@ for n = 1:lengthSound
     alpha = 4 / (2 + omega0^2 * k^2 + sig * k);
     beta = (sig * k - 2 - omega0^2 * k^2) / (2 + omega0^2 * k^2 + sig * k);
     epsilon = 2 * Sr * k^2 / (M * (2 + omega0^2 * k^2 + sig * k));
-    
-    yNext(n) = alpha * y + beta * yPrev + epsilon * deltaP;
+
+    yNext(n) = alpha * y + beta * yPrev + epsilon * deltaP + 2 * omega0^2 * k^2 * H0 / (2 + omega0^2 * k^2 + sig * k);
     
     Ub = w * subplus(y + H0) * sign(deltaP) * sqrt(2 * abs(deltaP)/rho);
     Ur = Sr * 1/(2*k) * (yNext(n) - yPrev);
@@ -148,7 +154,7 @@ for n = 1:lengthSound
     pNext(pRange) = p(pRange) - rho * c * lambda ./ SBar(pRange) .* (SHalf(pRange) .* vNext(pRange) - SHalf(pRange-1) .* vNext(pRange-1));
     pNext(1) = p(1) - rho * c * lambda ./ SBar(1) .* (-2 * (Ub + Ur) + 2 * SHalf(1) * vNext(1));
 
-    pNext(N) = p(N) - rho * c * lambda ./ SBar(N) .* (-2 * SHalf(end) .* vNext(end));
+%     pNext(N) = p(N) - rho * c * lambda ./ SBar(N) .* (-2 * SHalf(end) .* vNext(end));
     
     % set output from output position
     out(n) = p(outputPos);
@@ -157,8 +163,8 @@ for n = 1:lengthSound
     kinEnergy(n) = 1/(2 * rho * c^2) * h * sum(SBar .* scaling .* p.^2);
     potEnergy(n) = rho / 2 * h * sum(SHalf .* vNext .* v);
     
-    hReed(n) = M / 2 * ((1/k * (y - yPrev))^2 + omega0^2 * (y^2 + yPrev^2) / 2);
-    qReed(n) = M * sig * (1/(2*k) * (yNext(n) - yPrev))^2 + w * subplus(y + H0) * sqrt(2 / rho) * abs(deltaP)^(3/2);
+    hReed(n) = M / 2 * ((1/k * (y - yPrev))^2 + H0^2 * omega0^2 * (y^2 + yPrev^2) / 2);
+    qReed(n) = M * sig * (1/(2*k) * (yNext(n) - yPrev))^2 + Ub * deltaP; % + w * subplus(y + H0) * sqrt(2 / rho) * abs(deltaP)^(3/2);
     idx = n - (1 * (n~=1));
     qHReed(n) = k * qReed(n) + qHReed(idx);
     pReed(n) = -(Ub + Ur) * Pm;
@@ -176,7 +182,7 @@ for n = 1:lengthSound
 %         plot(sqrt(S), 'k');
 %         plot(-sqrt(S), 'k');
         xlim([1 N]);
-        
+        scatter(1, y * 10000)
 %         ylim([-max(sqrt(S)) max(sqrt(S))] * 1.1);
         title("Pressure");
         
@@ -210,15 +216,15 @@ end
 plot(yNext)
 
 function [S, SHalf, SBar] = setTube(N)
-    mp = linspace(0.0005, 0.0005, floor(N/20));       % mouthpiece
-    m2t = linspace(0.0005, 0.0001, floor(N/20));     % mouthpiece to tube
+    mp = linspace(0.0005, 0.0005, floor(N/40));       % mouthpiece
+    m2t = linspace(mp(end), 0.0001, floor(N/40));     % mouthpiece to tube
     alpha = 0.25;
-    b = m2t(end) * exp(alpha * (0:18));               % bell
+    b = m2t(end) * exp(alpha * (0:25));               % bell
     pointsLeft = N - length([mp, m2t, b]);
     tube = linspace(m2t(end), m2t(end), pointsLeft);        % tube
 
     S = [mp, m2t, tube, b]';                        % True geometry
-    S = 5000 * ones(N,1);
+%     S = 5000 * ones(N,1);
     % Calculate approximations to the geometry
     SHalf = (S(1:N-1) + S(2:N)) * 0.5;           	% mu_{x+}
     SBar = (SHalf(1:end-1) + SHalf(2:end)) * 0.5;

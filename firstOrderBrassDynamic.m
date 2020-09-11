6825a584a608cfd9ed588024ff517014798a6678
SilvinWillemsen <silvinwillemsen@gmail.com>
2020-09-11 11:04:31 +0200

    Working changing grid with only interpolating pressure.

--- firstOrderBrassDynamic.m
+++ firstOrderBrassDynamic.m
@@ -7,6 +7,7 @@ close all;
 
 % drawing variables
 drawThings = true;
+drawStart = 1;
 drawSpeed = 1;
 centered = true;
 
@@ -17,6 +18,7 @@ lengthSound = fs * 2;   % Duration (s)
 %% viscothermal effects
 T = 26.85;
 [c, rho, eta, nu, gamma] = calcThermoDynConstants (T);
+cInit = c;
 
 %% Tube variables
 h = c * k;              % Grid spacing (m)
@@ -67,19 +69,13 @@ totEnergyW = zeros (lengthSound, 1);
 totEnergy = zeros (lengthSound, 1);
 scaledTotEnergy = zeros (lengthSound, 1);
 
-kinScalingU = ones(length(up),1);
-kinScalingU(1) = 1 / 2;
-kinScalingU(end) = 1 / 2;
+potScalingU = ones(length(up),1);
+potScalingU(1) = 1 / 2;
+potScalingU(end) = 1 / 2;
 
-kinScalingW = ones(length(wp),1);
-kinScalingW(1) = 1 / 2;
-kinScalingW(end) = 1 / 2;
-
-potScalingU = ones(length(uv),1);
-% potScalingU(end) = 0.5;
-
-potScalingW = ones(length(wv),1);
-% potScalingW(1) = 0.5;
+potScalingW = ones(length(wp),1);
+potScalingW(1) = 1 / 2;
+potScalingW(end) = 1 / 2;
 
 ip = [alf * (alf - 1) * (alf - 2) / -6, ...
                 (alf - 1) * (alf + 1) * (alf - 2) / 2, ...
@@ -87,62 +83,152 @@ ip = [alf * (alf - 1) * (alf - 2) / -6, ...
                 alf * (alf + 1) * (alf - 1) / 6];
          
 SBarI = SBar(length(uv)-1:length(uv)+2) .* ip';
-wpInterpPrev = 0;
-uvInterpPrev = 0;
+uvMph = 0;
+wvmh = 0;
 
+changeC = true;
 for n = 1:lengthSound
     
+    % change wave speed
+    if changeC
+        c = cInit * (1+18 * n/fs);%* sin(0.5 * pi * n/fs));
+    else
+        c = c;
+    end
+    cSave(n) = c;
+    
+    % save previous state for comparison later
+    NPrev = N;
+
+    % recalculate gridspacing, points lambda^2 and alpha from new wave speed
+    h = c*k;
+    Ninit = L/h;
+    N = floor(L/h);
+    Nsave(n) = N;
+    hSave(n) = h;
+    
+    lambda = c * k / h;
+
+    alf = (Ninit - N);
+  
+    % calculate interpolator
+%     if interpol == "cubic"
+        ip = [alf * (alf - 1) * (alf - 2) / -6, ...
+                (alf - 1) * (alf + 1) * (alf - 2) / 2, ...
+                alf * (alf + 1) * (alf - 2) / -2, ...
+                alf * (alf + 1) * (alf - 1) / 6];
+%     else
+%         ip = [0, (1-alf), alf, 0];
+%     end
+
+    if abs(N - NPrev) > 1
+        disp('too fast')
+    end
+    
+    % add point if N^n > N^{n-1}
+    if N > NPrev
+        customIp = [-alf * (alf + 1) / ((alf + 2) * (alf + 3)), ...
+                    2 * alf * (alf + 1) / ((alf + 1) * (alf + 2)), ...
+                    2 * (alf + 1) / ((alf + 2) * (alf + 1)), ...
+                    -2 * alf / ((alf + 3) * (alf + 2))];
+        if mod(N,2) == 1
+%             uvNext = [uvNext; (ip(4) * uvNext(end-1) + ip(3) * uvNext(end) + ip(2) * wvNext(1) + ip(1) * wvNext(2))];
+%             uv = [uv; (ip(4) * uv(end-1) + ip(3) * uv(end) + ip(2) * wv(1) + ip(1) * wv(2))];
+            uvNext = [uv; uvNextMph];
+            uv = [uv; uvMph];
+            uvMph =  customIp * [uv(end-1:end); wv(1:2)];% should be properly interpolated
+            upNext = [upNext; customIp * [upNext(end-1:end); wpNext(1:2)]];
+            up = [up; customIp * [up(end-1:end); wp(1:2)]];
+            
+        else 
+%             wvNext = [(ip(1) * uvNext(end-1) + ip(2) * uvNext(end) + ip(3) * wvNext(1) + ip(4) * wvNext(2)); wvNext];
+%             wv = [(ip(1) * uv(end-1) + ip(2) * uv(end) + ip(3) * wv(1) + ip(4) * wv(2)); wv];
+            wvNext = [wvNextmh; wvNext];
+            wv = [wvmh; wv];
+            wvmh =  fliplr(customIp) * [uv(end-1:end); wv(1:2)];
+            wpNext = [(customIp(4) * upNext(end-1) + customIp(3) * upNext(end) + customIp(2) * wpNext(1) + customIp(1) * wpNext(2)); wpNext];
+            wp = [(customIp(4) * up(end-1) + customIp(3) * up(end) + customIp(2) * wp(1) + customIp(1) * wp(2)); wp];
+               
+        end
+        [S, SHalf, SBar] = setTube(N+1);
+        % insert matrix creation here
+        
+        potScalingU = ones(length(up),1);
+        potScalingW = ones(length(wp),1);
+        potScalingU(1) = 0.5;
+        potScalingW(end) = 0.5;
+        potScalingU(end) = 0.5;
+        potScalingW(1) = 0.5;
+    end   
+    
+    % remove point if N^n < N^{n-1}
+    if N < NPrev
+        if mod(N,2) == 0
+            uvMph =  uv(end);
+            uvNext = uvNext(1:end-1);
+            uv = uv(1:end-1);
+            upNext = upNext(1:end-1);
+            up = up(1:end-1);
+
+        else 
+            wvmh = wv(1);
+            wvNext = wvNext(2:end);
+            wv = wv(2:end);
+            wpNext = wpNext(2:end);
+            wp = wp(2:end);
+
+        end
+        [S, SHalf, SBar] = setTube(N+1);
+        
+        potScalingU = ones(length(up),1);
+        potScalingW = ones(length(wp),1);
+        potScalingU(1) = 0.5;
+        potScalingW(end) = 0.5;
+        potScalingU(end) = 0.5;
+        potScalingW(1) = 0.5;
+
+    end
+    upRange = 2:length(up)-1;         % range without boundaries
+    wpRange = 2:length(wp)-1;
+
+    A = [1, -ip(4); ...
+         -ip(4), 1];
+    v = [ip(1) * up(end-2) + ip(2) * up(end-1) + ip(3) * up(end); ...
+         ip(3) * wp(1) + ip(2) * wp(2) + ip(1) * wp(3)];
+    solut = A \ v;
+    
     %% Calculate velocities
-    wpInterp = alf * wp(1) + (1-alf) * wp(2);
-
-    uvNext(1:end) = uv(1:end) - lambda / (rho * c) * (up(2:end) - up(1:end-1));
-%     A = [1, -ip(4), 0, 0, 0; ...
-%          1, 0, 0, -rho * c * lambda / SBarI * SHalf(length(uv)), rho * c * lambda / SBarI * SHalf(length(uv)+1); ...
-%          0, 0, 1, -ip(3), -ip(4); ...
-%          0, -lambda / (rho * c), 1, 0, 0];
-%     v = [ip(1) * wp(3) + ip(2) * wp(2) + ip(3) * wp(1); ...
-%          wpInterpPrev; ...
-%          ip(1) * uvNext(end-2) + ip(2) * uvNext(end-1); ...
-%          uv(end) + lambda / (rho * c) * up(end); ...
-%          uvInterpPrev - lambda / (rho * c) * wp(1)];
-%      
-%     solut = A \ v;
-%     wpInterp = solut(1);
-%     wpMin1 = solut(2);
-%     uvInterp = solut(3);
-%     uvNext(end) = solut(4);
-%     uvMph = solut(5);
-%     check = uv(end) - lambda / (rho * c) * (wpInterp - up(end));
-%     uvNext(end) - check
-%     uvNext(end) = uv(end) - lambda / (rho * c) * (wpInterp - up(end));
-    wvNext(1:end) = wv(1:end) - lambda / (rho * c) * (wp(2:end) - wp(1:end-1));
-%     wvNext(1) = wv(1) - lambda / (rho * c) * (wp(2) - wp(1));
-
+    uvNext = uv - lambda / (rho * c) * (up(2:end) - up(1:end-1));
+    uvNextMph = uvMph - lambda / (rho * c) * (solut(2) - up(end));
+    
+    wvNext = wv - lambda / (rho * c) * (wp(2:end) - wp(1:end-1));
+    wvNextmh = wvmh - lambda / (rho * c) * (wp(1) - solut(1));
+    
     %% Calculate pressure
     uvInterp = (1 - alf) * uvNext(end-1) + alf * uvNext(end);
 
     upNext(upRange) = up(upRange) - rho * c * lambda ./ SBar(upRange) .* (SHalf(upRange) .* uvNext(upRange) - SHalf(upRange-1) .* uvNext(upRange-1));
-    upNext(end) = up(end) - rho * c * lambda ./ SBar(length(up)) .* (SHalf(length(up)) .* wvNext(1) - SHalf(length(up) - 1) .* uvNext(end));
+    upNext(end) = up(end) - rho * c * lambda ./ SBar(length(up)) .* (SHalf(length(up)) .* uvNextMph - SHalf(length(up) - 1) .* uvNext(end));
     
     wpNext(wpRange) = wp(wpRange) - rho * c * lambda ./ SBar(wpRange + length(up) - 1) .* (SHalf(wpRange + length(up) - 1) .* wvNext(wpRange) - SHalf(wpRange + length(up) - 2) .* wvNext(wpRange-1));
-    wpNext(1) = wp(1) - rho * c * lambda ./ SBar(length(up)) .* (SHalf(length(up)) .* wvNext(1) - SHalf(length(up) - 1) .* uvNext(end));
+    wpNext(1) = wp(1) - rho * c * lambda ./ SBar(length(up)) .* (SHalf(length(up)) .* wvNext(1) - SHalf(length(up) - 1) .* wvNextmh);
 
     %% Set output from output position
     out(n) = wp(end-5);
     
     %% Energies
-    kinEnergyU(n) = 1/(2 * rho * c^2) * h * sum (SBar(1:length(up)) .* kinScalingU .* up.^2);
-    potEnergyU(n) = rho / 2 * h * sum(SHalf(1:length(uv)) .* potScalingU .* uvNext .* uv);
+    kinEnergyU(n) = rho / 2 * h * sum(SHalf(1:length(uv)) .* uvNext .* uv);
+    potEnergyU(n) = 1/(2 * rho * c^2) * h * sum (SBar(1:length(up)) .* potScalingU .* up.^2);
     totEnergyU(n) = kinEnergyU(n) + potEnergyU(n);
     
-    kinEnergyW(n) = 1/(2 * rho * c^2) * h * sum (SBar(length(up):end) .* kinScalingW .* wp.^2);
-    potEnergyW(n) = rho / 2 * h * sum(SHalf(length(uv)+1:end) .* potScalingW .* wvNext .* wv);
+    kinEnergyW(n) = rho / 2 * h * sum(SHalf(length(uv)+1:end) .* wvNext .* wv);
+    potEnergyW(n) = 1/(2 * rho * c^2) * h * sum (SBar(length(up):end) .* potScalingW .* wp.^2);
     totEnergyW(n) = kinEnergyW(n) + potEnergyW(n);
 
     totEnergy(n) = totEnergyU(n) + totEnergyW(n);
 
     %% Draw things
-    if drawThings && mod (n, drawSpeed) == 0
+    if drawThings && mod (n, drawSpeed) == 0 && n > drawStart
         hLocsLeft = (0:(length(up))-1) * h;
         hLocsRight = flip(L - ((0:(length(wp)-1)) * h));        
         
@@ -150,15 +236,16 @@ for n = 1:lengthSound
         subplot(2,1,1)
         cla
         hold on;
-        plot(hLocsLeft * N / L, up, '-o');
-        plot(hLocsRight * N / L, wp, '-o');
-        plot(hLocsLeft(1:end-1) * N / L + 0.5, uvNext * 100, 'Marker', '.', 'MarkerSize', 10);
-        plot(hLocsRight(2:end) * N / L -0.5, wvNext * 100, 'Marker', '.', 'MarkerSize', 10);
-
+        plot(hLocsLeft / L, up, '-o');
+        plot(hLocsRight / L, wp, '-o');
+        plot(hLocsLeft(1:end) / L + 0.5 / N, [uvNext; uvNextMph] * 100, 'Marker', '.', 'MarkerSize', 10);
+        plot(hLocsRight(1:end) / L - 0.5 / N, [wvNextmh; wvNext] * 100, 'Marker', '.', 'MarkerSize', 10);
+%         xlim([0.4 0.6])
+        ylim([-1 1])
         % Plot scaled energy
         subplot(2,1,2)
         plot(totEnergy(1:n) / totEnergy(1) - 1)
-        pause(0.1);
+        pause(0.05);
         drawnow;
         
     end
@@ -170,21 +257,22 @@ for n = 1:lengthSound
     wv = wvNext;
     wp = wpNext;
 
-    wpInterpPrev = wpInterp;
+    uvMph = uvNextMph;
+    wvmh = wvNextmh;
 
 end   
 
 function [S, SHalf, SBar] = setTube(N)
-    mp = linspace(0.001, 0.001, floor(N/40));           % mouthpiece
-    m2t = linspace(mp(end), 0.0005, floor(N/40));       % mouthpiece to tube
-    
-    alpha = 0.25;                                       % bell
-    b = m2t(end) * exp(alpha * (0:18));
-    pointsLeft = N - length([mp, m2t, b]);
-    tube = linspace(m2t(end), m2t(end), pointsLeft);    % tube
+%     mp = linspace(0.001, 0.001, floor(N/40));           % mouthpiece
+%     m2t = linspace(mp(end), 0.0005, floor(N/40));       % mouthpiece to tube
+%     
+%     alpha = 0.25;                                       % bell
+%     b = m2t(end) * exp(alpha * (0:18));
+%     pointsLeft = N - length([mp, m2t, b]);
+%     tube = linspace(m2t(end), m2t(end), pointsLeft);    % tube
 
-    S = [mp, m2t, tube, b]';                            % True geometry
-
+%     S = [mp, m2t, tube, b]';                            % True geometry
+    S = ones(N, 1);
     % Calculate approximations to the geometry
     SHalf = (S(1:N-1) + S(2:N)) * 0.5;                  % mu_{x+}
     SBar = (SHalf(1:end-1) + SHalf(2:end)) * 0.5;
